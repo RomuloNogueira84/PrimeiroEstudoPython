18e46d6042ada48c5a38a603b3d2ad274edbb8c9
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-09-05 11:02:10 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,7 +1,8 @@
 # Função print()
 # A função print() é uma das funções mais importantes e usadas na linguagem Python.
 # Sua função é basicamente ,exibir mensagens na tela ou enviá-las para outro dispositivo,como imprimir dentro de arquivos de texto.
-# No Python 3, print() é uma função interna, de modo que não é necessario importar nenhuma  biblioteca para poder utilizá-la. Basta chamá-la e passar os argumentos necessários.
+# No Python 3, print() é uma função interna,
+# de modo que não é necessario importar nenhuma biblioteca para poder utilizá-la. Basta chamá-la e passar os argumentos necessários.
 
 print('Meu primeiro print!')
 print("Meu segundo print!")
@@ -20,7 +21,8 @@ print(10/10)
 print(10**10)
 # Fala, galera! Beleza? Hoje vamos falar sobre a função print() no Python, que é uma das mais usadas e importantes.
 # A função print() serve basicamente para exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto.
-# No Python 3, print() é uma função interna, então não precisa importar nenhuma biblioteca para usá-la. É só chamar a função e passar os argumentos necessários.
+# No Python 3, print() é uma função interna, então não precisa importar nenhuma biblioteca para usá-la.
+# É só chamar a função e passar os argumentos necessários.
 # Vamos ver uns exemplos maneiros:
 # Então, é isso aí! Com a função print(), a gente consegue mostrar na tela tudo o que a gente quiser de uma forma bem tranquila.
 # Agora é só botar a mão na massa e fazer aquele código maneiro! Valeu, galera!
