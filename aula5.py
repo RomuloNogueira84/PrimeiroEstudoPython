3d3eec4fa4c59f3fc02a6240033065ad4879be96
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-09-10 22:00:09 -0300

    Python5variaveis

--- aula5.py
+++ aula5.py
@@ -3,4 +3,46 @@
 #  Através da utilização de variáveis, podemos armazenar dados na memória do computador para acessa-los mais tarde,quando forem necessários.
 #Declaração e atribuição de variáveis
 # Algumas regras devem ser seguidas ao criarmos um nome para uma variável:
+# 1- precisa começar com uma letra ou um underscore
+# 2- não pode começar com um número
+# 3- não pode conter caracteres especiais (*, $, %, #, @...etc...)
+# 4- nomes de variáveis são case sensitives (nome é diferente de Nome)
+# 5- não pode ser uma palavra reservada da linguagem
 
+nome_aluno = "Romulo"
+print(nome_aluno)
+
+ano_de_nascimento_Romulo = 1984
+
+
+Nome_aluno = "Keity"
+ano_de_nascimento_Keity = 1995
+
+print(nome_aluno)
+print(Nome_aluno)
+print(ano_de_nascimento_Romulo)
+print(ano_de_nascimento_Keity)
+
+# Tipos de variáveis
+# Inteiros
+
+numero_telefone = 21987654321 
+type(numero_telefone)
+
+numero_cpf = 47446578901
+type(numero_cpf)
+
+#Floats
+altura = 1.87
+peso = 98.9
+type(altura)
+type(peso)
+
+# Strings
+
+frase = "Estou buscando aprender Python"
+type(frase)
+
+basico_string = "Uma string é simplesmente uma sequência de caracteres. Você pode criar uma string em Python usando aspas simples  ou duplas "
+
+type(basico_string)
