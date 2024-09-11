3269b850f4e1ee2b11b79e234525b2c8359c3421
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-09-10 23:03:30 -0300

    Python6

---
+++ aula6.py
@@ -0,0 +1,52 @@
+# Operadores relacionais
+# > maior é quando a primeira variável é maior que a segunda
+# < menor é quando a primeira variável é menor que a segunda
+# == igual é quando a primeira variável é igual a segunda
+# >= maior igual é quando a primeira variável é maior ou igual a segunda
+# <= menor igual é quando a primeira variável é menor ou igual a segunda
+# != diferente é quando a primeira variável é diferente da segunda
+
+nome_aluno = "Romulo"
+nome_aluno2 = "Romulo"
+print("Usando Romulo e ele mesmo 👦🏻👦🏻")
+print(nome_aluno > nome_aluno2)
+print(nome_aluno < nome_aluno2)
+print(nome_aluno == nome_aluno2)
+print(nome_aluno >= nome_aluno2)
+print(nome_aluno <= nome_aluno2)
+print(nome_aluno != nome_aluno2)
+
+print("Usando mãe Jane e filha Keity 👧🏻👩🏻")
+
+nome_aluno = "Jane"
+nome_aluno2 = "Keity"
+
+print(nome_aluno > nome_aluno2)
+print(nome_aluno < nome_aluno2)
+print(nome_aluno == nome_aluno2)
+print(nome_aluno >= nome_aluno2)
+print(nome_aluno <= nome_aluno2)
+print(nome_aluno != nome_aluno2)
+
+print("Com números")
+print (10>5 and 10<5)
+print((16+16+32+64) and (512/2 -128) == 128)
+print( (8*2) and + 16 == 64  )
+print("Python" != "Java" or "Java" == "Java")  
+
+#Operadores lógicos
+# and é o operador lógico AND
+# or é o operador lógico OR
+# not é o operador lógico NOT
+
+True = not True
+False = not False
+True = not False
+False = not True
+
+print(True and False) # False
+print(True and True) # True
+
+
+
+
